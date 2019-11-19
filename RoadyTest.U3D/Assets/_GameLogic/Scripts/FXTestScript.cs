01141bb5e918542972eab8cffe3b393a4f83fae4
Gavin <gavinariddle@gmail.com>
2019-11-18 17:38:13 -0800

    all of my stuff I added!

---
+++ RoadyTest.U3D/Assets/_GameLogic/Scripts/FXTestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXTestScript : MonoBehaviour
+{
+    private int damage = 0;
+    private bool stopped = true;
+    public ParticleSystem tireSmokeR;
+    public ParticleSystem tireSmokeL;
+    public ParticleSystem SmokeR;
+    public ParticleSystem SmokeL;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
